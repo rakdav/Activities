3ad4523146fcff27382cc13bfc838f7d76c6e9d7
rakdav <rakdav@inbox.ru>
2022-12-02 19:49:52 +0200

    last

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,31 +1,61 @@
 package com.example.myapplication;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import java.nio.channels.InterruptedByTimeoutException;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE="hello";
+    public static final String EXTRA_KEY="age";
     private Button button;
+    private EditText ageText;
+    private TextView textView;
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button=findViewById(R.id.button);
+        ageText=findViewById(R.id.age);
+        textView=findViewById(R.id.textView);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 Intent intent=new Intent(MainActivity.this,SecondActivity.class);
-                intent.putExtra(EXTRA_MESSAGE,"Hello, second");
-                startActivity(intent);
+                intent.putExtra(EXTRA_KEY,ageText.getText().toString());
+                startForResult.launch(intent);
             }
         });
     }
+    ActivityResultLauncher<Intent> startForResult=registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if(result.getResultCode()== Activity.RESULT_OK)
+                    {
+                        Intent intent=result.getData();
+                        String mes=intent.getStringExtra(EXTRA_MESSAGE);
+                        textView.setText(mes);
+                    }
+                    else
+                    {
+                        textView.setText("Error");
+                    }
+                }
+            }
+    );
 }
--- app/src/main/java/com/example/myapplication/SecondActivity.java
+++ app/src/main/java/com/example/myapplication/SecondActivity.java
@@ -3,21 +3,35 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity
 {
     private TextView text;
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-        text=findViewById(R.id.second);
+        text=findViewById(R.id.ageView);
+        button=findViewById(R.id.button2);
         Bundle arguments=getIntent().getExtras();
-        String name=arguments.getString(MainActivity.EXTRA_MESSAGE).toString();
-        text.setText(name);
+        String name=arguments.getString(MainActivity.EXTRA_KEY).toString();
+        text.setText("Возраст:"+name);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent data=new Intent();
+                data.putExtra(MainActivity.EXTRA_MESSAGE,"Вход разрешен");
+                setResult(RESULT_OK,data);
+                finish();
+            }
+        });
     }
 }
